e9f28463552dad291126396f65a1bd0d52592cfc
Mohamed Samir <43650702+abosamra0x90@users.noreply.github.com>
2025-11-26 16:13:46 +0200

    Update test_scan.py

--- src/python/strelka/scanners/test_scan.py
+++ src/python/strelka/scanners/test_scan.py
@@ -2,4 +2,4 @@ from strelka import strelka
 
 class TestScan(strelka.Scanner):
     def scan(self, data, file, options, expire_at):
-        self.event["helllllllllllllllllllllllllllllllloooooooooooooooooooooooooo"] = len(data)
+        self.event["test"] = "test"
