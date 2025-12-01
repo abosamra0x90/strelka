8fbfec6022a4556e280dd93cd0a4c5c01d9f6aba
Mohamed Samir <43650702+abosamra0x90@users.noreply.github.com>
2025-12-01 02:16:21 +0200

    Update strelka.py

--- src/python/strelka/strelka.py
+++ src/python/strelka/strelka.py
@@ -554,15 +554,16 @@ class Backend(object):
                         pipeline.rpush(f"event:{root_id}", format_event(event))
                         pipeline.expireat(f"event:{root_id}", expire_at)
                         pipeline.execute()
-                        if isinstance(event, bytes):
-                            event = json.loads(event.decode("utf-8"))
-                        elif isinstance(event, str):
-                            event = json.loads(event)
-                        webhook_url = "http://109.205.181.248:5001/webhook"
-                        name = file.name
-                        uuid_part, filename_part = name.split("___", 1)
-                        event["email.uuid"] = uuid_part
-                        requests.post(webhook_url,format_event(event),timeout=5)
+                        try:
+                            if isinstance(event, bytes):
+                                event = json.loads(event.decode("utf-8"))
+                            elif isinstance(event, str):
+                                event = json.loads(event)
+                            webhook_url = "http://109.205.181.248:5001/webhook"
+                            name = file.name
+                            uuid_part, filename_part = name.split("___", 1)
+                            event["email.uuid"] = uuid_part
+                            requests.post(webhook_url,format_event(event),timeout=5)
                     signal.alarm(0)
 
                 except DistributionTimeout:
